bcaa75740b7c7a159a2a18cae23d0df87050579c
Muhammad Faisal <digital.1faisal@gmail.com>
2024-12-15 16:40:55 +0000

    update favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulseSlow: "pulse 2s cubic-bezier(0,0,0.3,0.9) infinite",
+        pingSlow: "ping 2s cubic-bezier(0,0,0.3,0.9) infinite",
+        spinSlow: "spin 2s cubic-bezier(0,0,0.3,0.9) infinite",
+      },
+    },
   },
   plugins: [],
-}
-
+};
