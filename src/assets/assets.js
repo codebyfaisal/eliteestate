a94eca2c828cbbe4d5597cf2f4937bf177c83eb7
Muhammad Faisal <digital.1faisal@gmail.com>
2024-12-15 16:10:32 +0000

    update assets directory

---
+++ src/assets/assets.js
@@ -0,0 +1,98 @@
+import logo from "./logo.png";
+import logo_w from "./logo_w.png";
+import cross_icon from "./cross_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import location from "./location.svg";
+import comma from "./comma.svg";
+import home_1_icon from "./home_1.svg";
+import home_2_icon from "./home_2.svg";
+import home_3_icon from "./home_3.svg";
+import header_img from "./header_img.avif";
+import about_img from "./about_img.avif";
+import project_img_1 from "./project_img_1.avif";
+import project_img_2 from "./project_img_2.avif";
+import project_img_3 from "./project_img_3.avif";
+import project_img_4 from "./project_img_4.avif";
+import project_img_5 from "./project_img_5.avif";
+import project_img_6 from "./project_img_6.avif";
+import profile_img_1 from "./profile_img_1.png";
+import profile_img_2 from "./profile_img_2.png";
+import profile_img_3 from "./profile_img_3.png";
+
+export const assets = {
+  logo,
+  logo_w,
+  cross_icon,
+  menu_icon,
+  header_img,
+  about_img,
+  project_img_1,
+  project_img_2,
+  project_img_3,
+  project_img_4,
+  location,
+  comma,
+  home_1_icon,
+  home_2_icon,
+  home_3_icon,
+};
+
+export const projectsData = [
+  {
+    title: "Oceanview Escape",
+    price: "450,000",
+    location: "Miami, Florida",
+    image: project_img_1,
+  },
+  {
+    title: "Mountain Retreat",
+    price: "325,000",
+    location: "Aspen, Colorado",
+    image: project_img_2,
+  },
+  {
+    title: "Urban Loft",
+    price: "600,000",
+    location: "New York City, New York",
+    image: project_img_3,
+  },
+  {
+    title: "Countryside Cottage",
+    price: "275,000",
+    location: "Nashville, Tennessee",
+    image: project_img_4,
+  },
+  {
+    title: "Lakefront Villa",
+    price: "850,000",
+    location: "Lake Tahoe, California",
+    image: project_img_5,
+  },
+  {
+    title: "Desert Oasis",
+    price: "400,000",
+    location: "Phoenix, Arizona",
+    image: project_img_6,
+  },
+];
+
+export const testimonialsData = [
+  {
+    name: "Donald Jackman",
+    profession: "Real Estate Developer",
+    image: profile_img_1,
+    text: "As a real estate developer, finding the right properties is crucial. Their expertise and commitment to understanding my goals helped me secure some of the best deals in the market. I couldn't have asked for a better partner.",
+  },
+  {
+    name: "Richard Nelson",
+    profession: "Product Manager",
+    image: profile_img_2,
+    text: "Managing product development requires precise execution and constant attention to detail. They understood my needs and helped me find a perfect space for our growing team, making the entire process smooth and seamless.",
+  },
+  {
+    name: "James Washington",
+    profession: "Entrepreneur",
+    image: profile_img_3,
+    text: "As an entrepreneur, finding the right environment to grow my business is essential. Their personalized approach and industry knowledge made finding the perfect property a breeze. I trust them for all my future ventures.",
+  },
+];
